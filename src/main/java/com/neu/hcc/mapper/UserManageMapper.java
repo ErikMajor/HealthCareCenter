96a35f2248401c6ade471b850aa96b53f391130f
Timskt <1149177967@qq.com>
2021-09-02 11:15:57 +0800

    更新

--- src/main/java/com/neu/hcc/mapper/UserManageMapper.java
+++ src/main/java/com/neu/hcc/mapper/UserManageMapper.java
@@ -9,11 +9,13 @@ import java.util.List;
 public interface UserManageMapper {
     int deleteByPrimaryKey(Integer id);
 
-    int insert(UserManage record);
+    void insert(UserManage record);
 
     UserManage selectByPrimaryKey(Integer id);
 
     List<UserManage> selectAll();
 
     int updateByPrimaryKey(UserManage record);
+
+    List<UserManage> login(UserManage record);
 }
